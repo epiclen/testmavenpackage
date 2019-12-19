cf009cb8be63bd5c48e184b71da0213f27434019
liangzhao <liangzhao@alauda.io>
2019-12-19 17:45:29 +0800

    mmm

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,15 @@
-public class MainClass {
-    public static void main(String[] args){
-        String aa ="aaa/";
+import io.test.TestClass;
 
-        if (aa.lastIndexOf("/")==aa.length()-1){
-            aa=aa.substring(0,aa.length()-1);
+public class MainClass {
+    public static void main(String[] args) {
+        String aa = "aaa/";
+
+        TestClass tc = new TestClass();
+
+        tc.whoAmI();
+
+        if (aa.lastIndexOf("/") == aa.length() - 1) {
+            aa = aa.substring(0, aa.length() - 1);
             System.out.println(aa);
         }
     }
